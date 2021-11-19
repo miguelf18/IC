874e784c9a7ca6078a83d43ea0f08392bcfd4047
miguelf18 <49985557+miguelf18@users.noreply.github.com>
2021-11-19 19:05:14 +0000

    ex8 final

--- ex08/main.cpp
+++ ex08/main.cpp
@@ -11,38 +11,28 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    cout << "input audio file: " << argv[1] << endl;
-    cout << "output audio file: " << argv[2] << endl;
+    //cout << "input audio file: " << argv[1] << endl;
+    //cout << "output audio file: " << argv[2] << endl;
 
     AudioFile<double> inputFile;
     inputFile.load(argv[1]);
-    // inputFile.printSummary();
 
     int numChannels = inputFile.getNumChannels();
     int numSamples = inputFile.getNumSamplesPerChannel();
-    int bitDepth_input = inputFile.getBitDepth();
-
-    //cout << "Input bitDepth: " << bitDepth_input << endl;
 
     AudioFile<double> outputFile;
     outputFile.setNumChannels(numChannels);
     outputFile.setNumSamplesPerChannel(numSamples);
-    outputFile.setBitDepth(8);
-
-    // outputFile.printSummary();
-
-    int bitDepth_output = outputFile.getBitDepth();
-    //cout << "Output bitDepth: " << bitDepth_output << endl;
     
+    int nbits = 1;
+
     for (int i = 0; i < numChannels; i++)
     {
         for (int j = 0; j < numSamples; j++)
         {
-            double currentSample = inputFile.samples[i][j];
-            outputFile.samples[i][j] = currentSample;
+            int currentSample = inputFile.samples[i][j];
+            outputFile.samples[i][j] = (currentSample >> nbits) << nbits;
         }
     }
-    //inputFile.printSummary();
-    //outputFile.printSummary();
     outputFile.save(argv[2]);
 }
